c4f7bb79ba21e7642715c9a0180e1d6d7b71359d
Andy <ant@kaula.ru>
2017-03-16 15:37:25 +0300

    First commit

---
+++ KaulaTelegramBundle.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ant
+ * Date: 16.03.2017
+ * Time: 14:50
+ */
+
+namespace Kaula\TelegramBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KaulaTelegramBundle extends Bundle {
+
+
+
+}
