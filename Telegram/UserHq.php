30ca7f75b2a677f3f8e57d8e241d63757b5247ee
Andy <ant@kaula.ru>
2017-09-28 21:47:15 +0300

    Moved user resolve earlier to Update event.

--- Telegram/UserHq.php
+++ Telegram/UserHq.php
@@ -9,10 +9,11 @@
 namespace Kaula\TelegramBundle\Telegram;
 
 
-
 use Doctrine\Common\Collections\ArrayCollection;
+use Kaula\TelegramBundle\Entity\Role;
 use Kaula\TelegramBundle\Entity\User;
 use unreal4u\TelegramAPI\Telegram\Types\Update;
+use unreal4u\TelegramAPI\Telegram\Types\User as TelegramUser;
 
 class UserHq
 {
@@ -37,30 +38,94 @@ class UserHq
   }
 
   /**
+   * Formats user name.
+   *
+   * @param User $userEntity
+   * @param bool $redundantFormat Return both native and external names
+   * @return string
+   */
+  public static function formatUserName($userEntity, $redundantFormat = false)
+  {
+    if (!is_null($userEntity)) {
+      // User set
+      $user_name = trim(
+        $userEntity->getFirstName().' '.$userEntity->getLastName()
+      );
+      $external_name = trim(
+        $userEntity->getExternalFirstName().' '.
+        $userEntity->getExternalLastName()
+      );
+    } else {
+      // User not set
+      $user_name = 'не указан';
+      $external_name = '';
+    }
+
+    if (!empty($external_name)) {
+
+      // Return both native and external names
+      if ($redundantFormat) {
+        $user_name = sprintf(
+          '%s (%s)',
+          $user_name,
+          $external_name
+        );
+      } else {
+        $user_name = $external_name;
+      }
+
+    }
+
+    return $user_name;
+  }
+
+  /**
    * Finds current telegram user in the database and stores the object for
    * later use.
    *
    * @param Update $update
-   * @return null|User
+   * @return User
    */
   public function resolveCurrentUser(Update $update)
   {
-    $update_type = $this->getBot()
+    $updateType = $this->getBot()
       ->whatUpdateType($update);
+    $l = $this->getBot()
+      ->getLogger();
+    $l->debug(
+      'About to resolve current user for update type {update_type}',
+      ['update_type' => $updateType]
+    );
 
     // Assign telegram user object depending on update type
-    $telegram_user = null;
-    if (Bot::UT_MESSAGE == $update_type) {
+    $telegramUser = null;
+    if (Bot::UT_MESSAGE == $updateType) {
       if (!is_null($update->message)) {
-        $telegram_user = $update->message->from;
+        $telegramUser = $update->message->from;
       }
-    } elseif (Bot::UT_CALLBACK_QUERY == $update_type) {
+    } elseif (Bot::UT_CALLBACK_QUERY == $updateType) {
       if (!is_null($update->callback_query)) {
-        $telegram_user = $update->callback_query->from;
+        $telegramUser = $update->callback_query->from;
       }
     }
+    $l->debug(
+      'Telegram user ID: {telegram_id}',
+      [
+        'telegram_id'   => ($telegramUser) ? $telegramUser->id : 'not resolved',
+        'telegram_user' => $telegramUser,
+      ]
+    );
+    // Check if already resolved
+    if ($this->current_user) {
+      $l->debug('Already resolved', ['current_user' => $this->current_user]);
+      if ($telegramUser) {
+        $this->updateUserEntity($this->current_user, $telegramUser);
+      }
+
+      return $this->current_user;
+    }
     // Well?..
-    if (is_null($telegram_user)) {
+    if (is_null($telegramUser)) {
       return null;
     }
 
@@ -69,12 +134,32 @@ class UserHq
       $this->current_user = $this->getBot()
         ->getDoctrine()
         ->getRepository('KaulaTelegramBundle:User')
-        ->findOneBy(['telegram_id' => $telegram_user->id])
+        ->findOneBy(['telegram_id' => $telegramUser->id])
     )) {
-      return null;
+      $l->debug(
+        'Telegram user not found in the database with ID={telegram_id}, creating',
+        ['telegram_id' => $telegramUser->id]
+      );
+
+      // Get user entity by telegram user
+      $this->current_user = $this->createUser($telegramUser);
+      // Load anonymous roles and assign them to the user
+      $roles = $this->getAnonymousRoles();
+      $this->assignRoles($roles, $this->current_user);
+
+      // Commit changes
+      $this->getBot()
+        ->getDoctrine()
+        ->getManager()
+        ->flush();
     }
 
-    return $this->getCurrentUser();
+    $l->debug(
+      'Telegram user resolved to entity',
+      ['user' => $this->current_user]
+    );
+
+    return $this->current_user;
   }
 
   /**
@@ -86,13 +171,71 @@ class UserHq
   }
 
   /**
-   * Get database entity for current database user.
+   * Updates user entity.
    *
+   * @param User $user
+   * @param TelegramUser $telegramUser
+   */
+  private function updateUserEntity($user, $telegramUser)
+  {
+    // Update information
+    $user->setTelegramId($telegramUser->id)
+      ->setFirstName($telegramUser->first_name)
+      ->setLastName($telegramUser->last_name)
+      ->setUsername($telegramUser->username);
+  }
+
+  /**
+   * Returns the User object by telegram user.
+   *
+   * @param TelegramUser $telegramUser
    * @return User
    */
-  public function getCurrentUser()
+  private function createUser($telegramUser)
   {
-    return $this->current_user;
+    $user = new User();
+    $this->getBot()
+      ->getDoctrine()
+      ->getManager()
+      ->persist($user);
+    $this->updateUserEntity($user, $telegramUser);
+
+    return $user;
+  }
+
+  /**
+   * Returns array with roles for anonymous users.
+   *
+   * @return array
+   */
+  private function getAnonymousRoles()
+  {
+    $roles = $this->getBot()
+      ->getDoctrine()
+      ->getRepository('KaulaTelegramBundle:Role')
+      ->findBy(['anonymous' => true]);
+    if (0 == count($roles)) {
+      throw new \LogicException('Roles for guests not found');
+    }
+
+    return $roles;
+  }
+
+  /**
+   * Assigns specified roles to the user.
+   *
+   * @param array $roles
+   * @param User $user
+   */
+  private function assignRoles(array $roles, User $user)
+  {
+    /** @var Role $single_role */
+    foreach ($roles as $single_role) {
+      if (!$user->getRoles()
+        ->contains($single_role)) {
+        $user->addRole($single_role);
+      }
+    }
   }
 
   /**
@@ -108,6 +251,16 @@ class UserHq
 
     return $this->getCurrentUser()
       ->isBlocked();
+  }
+
+  /**
+   * Get database entity for current database user.
+   *
+   * @return User
+   */
+  public function getCurrentUser()
+  {
+    return $this->current_user;
   }
 
   /**
@@ -162,51 +315,9 @@ class UserHq
   {
     /** @var User $user */
     return $this->getBot()
-        ->getDoctrine()
-        ->getRepository('KaulaTelegramBundle:User')
-        ->findOneBy(['telegram_id' => $telegram_user->id]);
-  }
-
-  /**
-   * Formats user name.
-   *
-   * @param User $userEntity
-   * @param bool $redundantFormat Return both native and external names
-   * @return string
-   */
-  public static function formatUserName($userEntity, $redundantFormat = false)
-  {
-    if (!is_null($userEntity)) {
-      // User set
-      $user_name = trim(
-        $userEntity->getFirstName().' '.$userEntity->getLastName()
-      );
-      $external_name = trim(
-        $userEntity->getExternalFirstName().' '.
-        $userEntity->getExternalLastName()
-      );
-    } else {
-      // User not set
-      $user_name = 'не указан';
-      $external_name = '';
-    }
-
-    if (!empty($external_name)) {
-
-      // Return both native and external names
-      if ($redundantFormat) {
-        $user_name = sprintf(
-          '%s (%s)',
-          $user_name,
-          $external_name
-        );
-      } else {
-        $user_name = $external_name;
-      }
-
-    }
-
-    return $user_name;
+      ->getDoctrine()
+      ->getRepository('KaulaTelegramBundle:User')
+      ->findOneBy(['telegram_id' => $telegram_user->id]);
   }
 
 }
